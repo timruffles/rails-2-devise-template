b186431ede29501e07b54e8a386d3e029258a5a7
Tim Ruffles <timruffles@Tims-Laptop.lan>
2010-07-17 13:57:34 +0100

    Adding gems

--- devise.rb
+++ devise.rb
@@ -1,1 +1,7 @@
+gem 'thor'
+gem 'warden'
+gem 'devise', :version => '1.0.7'
+
+rake 'gems:install'
+
 inject_into_file 'README', '=== With added userness from a template', :after => "== Welcome to Rails\n", :verbose => true
